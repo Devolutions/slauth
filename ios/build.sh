40e63aeafb518bde69f2529d39c1929b9d1f3fc1
Richer Archambault <richer.arc@gmail.com>
2019-07-05 14:47:16 -0400

    build

--- ios/build.sh
+++ ios/build.sh
@@ -8,3 +8,12 @@ armv7-apple-ios \
 armv7s-apple-ios \
 x86_64-apple-ios \
 i386-apple-ios --release
+
+mkdir ./target/ios
+
+cp ./target/aarch64-apple-ios/release/libslauth.a ./target/ios/libslauth_arm64.a
+cp ./target/armv7-apple-ios/release/libslauth.a ./target/ios/libslauth_arm_v7.a
+cp ./target/armv7s-apple-ios/release/libslauth.a ./target/ios/libslauth_arm_v7s.a
+cp ./target/i386-apple-ios/release/libslauth.a ./target/ios/libslauth_i386.a
+cp ./target/universal/release/libslauth.a ./target/ios/libslauth_universal.a
+cp ./target/x86_64-apple-ios/release/libslauth.a ./target/ios/libslauth_x86.a
