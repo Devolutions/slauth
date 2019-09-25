f830546265f534819654ff1795bdb45e1179b2a9
Luc Fauvel <lfauvel@devolutions.net>
2019-09-25 14:19:09 -0400

    Added missing refrences

--- fuzz/fuzz_targets/fuzz_webauthn_messages.rs
+++ fuzz/fuzz_targets/fuzz_webauthn_messages.rs
@@ -2,6 +2,8 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate slauth;
 
+use slauth::webauthn::proto::raw_message::*;
+
 fuzz_target!(|data: &[u8]| {
     let _ = AttestationObject::from_bytes(data);
     let _ = AuthenticatorData::from_vec(data.to_vec());
