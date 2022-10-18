811b088c3164a6371e8d62c86e42326a91ed4332
Samuel Bergeron-Drouin <sbergerondrouin@devolutions.net>
2022-10-18 14:12:24 -0400

    WIP: Updated all dependancies, and added `cargo-deny` configuration
    Saphir 3.1.0 need to be published before we update this

--- examples/web-server.rs
+++ examples/web-server.rs
@@ -1,222 +1,224 @@
-pub fn main() {
-    #[cfg(feature = "webauthn-server")]
-    {
-        use saphir::{BasicController, Method, Middleware, RequestContinuation, Server, SyncRequest, SyncResponse};
-        use serde_json::json;
-        use slauth::webauthn::{
-            error::{CredentialError as CredE, Error::CredentialError},
-            proto::{
-                constants::WEBAUTHN_CHALLENGE_LENGTH,
-                raw_message::CredentialPublicKey,
-                web_message::{PublicKeyCredential, PublicKeyCredentialCreationOptions, PublicKeyCredentialRequestOptions},
-            },
-            server::{CredentialCreationBuilder, CredentialCreationVerifier, CredentialRequestBuilder, CredentialRequestVerifier},
-        };
-        use std::{collections::HashMap, str::FromStr, sync::RwLock};
-        use uuid::Uuid;
+use saphir::prelude::*;
+use serde_json::{json, Value};
+use slauth::webauthn::{
+    error::{CredentialError as CredE, Error::CredentialError},
+    proto::{
+        constants::WEBAUTHN_CHALLENGE_LENGTH,
+        raw_message::CredentialPublicKey,
+        web_message::{PublicKeyCredential, PublicKeyCredentialCreationOptions, PublicKeyCredentialRequestOptions},
+    },
+    server::{CredentialCreationBuilder, CredentialCreationVerifier, CredentialRequestBuilder, CredentialRequestVerifier},
+};
+use std::{collections::HashMap, sync::RwLock};
+use rand::seq::IteratorRandom;
+use uuid::Uuid;
 
-        let server = Server::builder();
+struct TestController {
+    creds: RwLock<HashMap<String, (CredentialPublicKey, u32)>>,
+    reg_contexts: RwLock<HashMap<String, PublicKeyCredentialCreationOptions>>,
+    sign_contexts: RwLock<HashMap<String, PublicKeyCredentialRequestOptions>>,
+}
 
-        let server = server
-            .configure_middlewares(|stack| stack.apply(CorsMiddleware::new(), vec!["/"], None))
-            .configure_router(|router| {
-                let basic_test_cont = BasicController::new("/webauthn", TestControllerContext::new());
-
-                basic_test_cont.add(Method::GET, "/register", TestControllerContext::register_request);
-
-                basic_test_cont.add(Method::POST, "/register", TestControllerContext::complete_register);
-
-                basic_test_cont.add(Method::GET, "/sign", TestControllerContext::sign_request);
-
-                basic_test_cont.add(Method::POST, "/sign", TestControllerContext::complete_sign);
-
-                router.add(basic_test_cont)
-            })
-            .configure_listener(|listener_config| listener_config.set_uri("http://0.0.0.0:12345"))
-            .build();
-
-        if let Err(e) = server.run() {
-            println!("{:?}", e);
-            panic!()
-        }
-
-        struct TestControllerContext {
-            creds: RwLock<HashMap<String, (CredentialPublicKey, u32)>>,
-            reg_contexts: RwLock<HashMap<String, PublicKeyCredentialCreationOptions>>,
-            sign_contexts: RwLock<HashMap<String, PublicKeyCredentialRequestOptions>>,
-        }
-
-        impl TestControllerContext {
-            pub fn new() -> Self {
-                TestControllerContext {
-                    creds: RwLock::new(HashMap::new()),
-                    reg_contexts: RwLock::new(HashMap::new()),
-                    sign_contexts: RwLock::new(HashMap::new()),
-                }
-            }
-
-            pub fn register_request(&self, _req: &SyncRequest, res: &mut SyncResponse) {
-                let uuid = base64::encode_config(
-                    Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
-                        .expect("should be ok")
-                        .as_bytes(),
-                    base64::URL_SAFE_NO_PAD,
-                );
-                let builder = CredentialCreationBuilder::new()
-                    .challenge(gen_challenge(WEBAUTHN_CHALLENGE_LENGTH))
-                    .user(uuid.clone(), "lfauvel@devolutions.net".to_string(), "Luc Fauvel".to_string(), None)
-                    .rp("localhost".to_string(), None, Some("localhost".to_string()))
-                    .build();
-
-                match builder {
-                    Ok(pubkey) => {
-                        if let Ok(mut contexts) = self.reg_contexts.write() {
-                            contexts.insert(uuid, pubkey.clone());
-                        }
-                        res.status(200)
-                            .body(serde_json::to_vec(&json!({ "publicKey": pubkey })).expect("This is valid json"))
-                            .header("Content-Type", "application/json");
-                    }
-                    Err(e) => {
-                        dbg!(e);
-                    }
-                }
-            }
-
-            pub fn complete_register(&self, req: &SyncRequest, _res: &mut SyncResponse) {
-                let value = serde_json::from_str::<PublicKeyCredential>(&String::from_utf8(req.body().clone()).unwrap());
-                let uuid = base64::encode_config(
-                    Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
-                        .expect("should be ok")
-                        .as_bytes(),
-                    base64::URL_SAFE_NO_PAD,
-                );
-                if let Ok(cred) = value {
-                    if let Some(context) = self.reg_contexts.read().expect("should be ok").get(&uuid) {
-                        let mut verifier = CredentialCreationVerifier::new(cred.clone(), context.clone(), "http://localhost");
-                        if let Ok(result) = verifier.verify() {
-                            self.creds.write().unwrap().insert(cred.id, (result.public_key, result.sign_count));
-                        }
-                    }
-                }
-            }
-
-            pub fn sign_request(&self, _req: &SyncRequest, res: &mut SyncResponse) {
-                let mut builder = CredentialRequestBuilder::new()
-                    .rp("localhost".to_string())
-                    .challenge(gen_challenge(WEBAUTHN_CHALLENGE_LENGTH));
-                let uuid = base64::encode_config(
-                    Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
-                        .expect("should be ok")
-                        .as_bytes(),
-                    base64::URL_SAFE_NO_PAD,
-                );
-                for (cred, _) in self.creds.read().unwrap().iter() {
-                    builder = builder.allow_credential(cred.clone());
-                }
-                match builder.build() {
-                    Ok(pubkey) => {
-                        self.sign_contexts.write().unwrap().insert(uuid, pubkey.clone());
-                        res.status(200)
-                            .body(serde_json::to_vec(&json!({ "publicKey": pubkey })).expect("This is valid json"))
-                            .header("Content-Type", "application/json");
-                    }
-                    Err(e) => {
-                        dbg!(e);
-                    }
-                }
-            }
-
-            pub fn complete_sign(&self, req: &SyncRequest, res: &mut SyncResponse) {
-                let value = serde_json::from_str::<PublicKeyCredential>(&String::from_utf8(req.body().clone()).unwrap());
-                let uuid = base64::encode_config(
-                    Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
-                        .expect("should be ok")
-                        .as_bytes(),
-                    base64::URL_SAFE_NO_PAD,
-                );
-                let result = if let Ok(cred) = value {
-                    if let Some(context) = self.sign_contexts.read().expect("should be ok").get(&uuid) {
-                        if let Some((cred_pub, sign_count)) = self.creds.read().unwrap().get(&cred.id) {
-                            let mut verifier = CredentialRequestVerifier::new(
-                                cred.clone(),
-                                cred_pub.clone(),
-                                context.clone(),
-                                "http://localhost",
-                                uuid.as_str(),
-                                *sign_count,
-                            );
-                            match verifier.verify() {
-                                Ok(res) => Ok((cred_pub.clone(), res.sign_count)),
-
-                                Err(e) => Err(e),
-                            }
-                        } else {
-                            Err(CredentialError(CredE::Other("Credential not found".to_string())))
-                        }
-                    } else {
-                        Err(CredentialError(CredE::Other("Context not found".to_string())))
-                    }
-                } else {
-                    Err(CredentialError(CredE::Other(
-                        "Public key credential could not be parsed".to_string(),
-                    )))
-                };
-
-                match result {
-                    Ok((cred_pub, sign_count)) => {
-                        self.creds.write().unwrap().insert(uuid, (cred_pub, sign_count));
-                        res.status(200).body("it works".to_string());
-                    }
-
-                    Err(e) => {
-                        res.status(500).body(e.to_string());
-                    }
-                }
-            }
-        }
-
-        pub struct CorsMiddleware {}
-
-        impl CorsMiddleware {
-            pub fn new() -> Self {
-                CorsMiddleware {}
-            }
-        }
-
-        impl Middleware for CorsMiddleware {
-            fn resolve(&self, req: &mut SyncRequest, res: &mut SyncResponse) -> RequestContinuation {
-                let headers = req.headers_map();
-                if let Some(Ok(origin)) = headers.get("Origin").map(|c| c.to_str()) {
-                    res.header("Access-Control-Allow-Origin", origin);
-                } else {
-                    res.header("Access-Control-Allow-Origin", "*");
-                }
-
-                res.header("Access-Control-Expose-Headers", "Auth-ID, WWW-Authenticate, auth-id, www-authenticate, authorization, Authorization, Origin, origin, Set-Cookie, set-cookie, Cookie, cookie");
-
-                if req.uri().path().contains("/auth") {
-                    res.header("Access-Control-Allow-Credentials", "true");
-                }
-
-                if req.method() == Method::OPTIONS.as_ref() {
-                    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                    res.header("Access-Control-Allow-Headers", "Auth-ID, WWW-Authenticate, auth-id, www-authenticate, authorization, Authorization, Origin, origin, Set-Cookie, set-cookie, Cookie, cookie, Code");
-                    return RequestContinuation::Stop;
-                }
-
-                RequestContinuation::Continue
-            }
-        }
-
-        pub fn gen_challenge(len: usize) -> String {
-            use rand::{thread_rng, Rng};
-
-            let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            let mut rng = thread_rng();
-            let value = (0..len).map(|_| *rng.choose(charset.as_ref()).unwrap() as u8).collect::<Vec<u8>>();
-            base64::encode_config(value.as_slice(), base64::URL_SAFE_NO_PAD)
+impl TestController {
+    pub fn new() -> Self {
+        TestController {
+            creds: RwLock::new(HashMap::new()),
+            reg_contexts: RwLock::new(HashMap::new()),
+            sign_contexts: RwLock::new(HashMap::new()),
         }
     }
+}
+
+#[derive(Debug)]
+enum TestError {
+    Slauth(slauth::webauthn::error::Error),
+    Internal,
+}
+
+impl From<slauth::webauthn::error::Error> for TestError {
+    fn from(e: slauth::webauthn::error::Error) -> Self {
+        TestError::Slauth(e)
+    }
+}
+
+impl Responder for TestError {
+    fn respond_with_builder(self, builder: Builder, _ctx: &HttpContext) -> Builder {
+        match self {
+            TestError::Slauth(_) => builder.status(500),
+            TestError::Internal => builder.status(500),
+        }
+    }
+}
+
+#[controller(name = "webauthn")]
+impl TestController {
+    #[get("/register")]
+    async fn register_request(&self) -> Result<Json<Value>, TestError> {
+        let uuid = base64::encode_config(
+            Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
+                .expect("should be ok")
+                .as_bytes(),
+            base64::URL_SAFE_NO_PAD,
+        );
+        let builder = CredentialCreationBuilder::new()
+            .challenge(gen_challenge(WEBAUTHN_CHALLENGE_LENGTH))
+            .user(uuid.clone(), "lfauvel@devolutions.net".to_string(), "Luc Fauvel".to_string(), None)
+            .rp("localhost".to_string(), None, Some("localhost".to_string()))
+            .build();
+
+        match builder {
+            Ok(pubkey) => {
+                if let Ok(mut contexts) = self.reg_contexts.write() {
+                    contexts.insert(uuid, pubkey.clone());
+                }
+                Ok(Json(json!({ "publicKey": pubkey })))
+            }
+            Err(e) => {
+                dbg!(e);
+                Err(TestError::Internal)
+            }
+        }
+    }
+
+    #[post("/register")]
+    async fn complete_register(&self, cred: Json<PublicKeyCredential>) -> () {
+        let cred = cred.into_inner();
+        let uuid = base64::encode_config(
+            Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
+                .expect("should be ok")
+                .as_bytes(),
+            base64::URL_SAFE_NO_PAD,
+        );
+        if let Some(context) = self.reg_contexts.read().expect("should be ok").get(&uuid) {
+            let mut verifier = CredentialCreationVerifier::new(cred.clone(), context.clone(), "http://localhost");
+            if let Ok(result) = verifier.verify() {
+                self.creds.write().unwrap().insert(cred.id, (result.public_key, result.sign_count));
+            }
+        }
+    }
+
+    #[get("/sign")]
+    async fn sign_request(&self) -> Result<Json<Value>, TestError> {
+        let mut builder = CredentialRequestBuilder::new()
+            .rp("localhost".to_string())
+            .challenge(gen_challenge(WEBAUTHN_CHALLENGE_LENGTH));
+        let uuid = base64::encode_config(
+            Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
+                .expect("should be ok")
+                .as_bytes(),
+            base64::URL_SAFE_NO_PAD,
+        );
+        for (cred, _) in self.creds.read().unwrap().iter() {
+            builder = builder.allow_credential(cred.clone());
+        }
+        match builder.build() {
+            Ok(pubkey) => {
+                self.sign_contexts.write().unwrap().insert(uuid, pubkey.clone());
+                Ok(Json(json!({ "publicKey": pubkey })))
+            }
+            Err(e) => {
+                dbg!(e);
+                Err(TestError::Internal)
+            }
+        }
+    }
+
+    #[post("/sign")]
+    async fn complete_sign(&self, req: Json<PublicKeyCredential>) -> Result<(u16, String), TestError> {
+        let cred = req.into_inner();
+        let uuid = base64::encode_config(
+            Uuid::from_str("e1aea4d6-d2ee-4218-9f1c-5ccddadaa1a7")
+                .expect("should be ok")
+                .as_bytes(),
+            base64::URL_SAFE_NO_PAD,
+        );
+
+        let ctx_lock = self.sign_contexts.read()
+            .map_err(|_| CredentialError(CredE::Other("Synchronization error".to_string())))?;
+        let context = ctx_lock.get(&uuid)
+            .ok_or(CredentialError(CredE::Other("Context not found".to_string())))?;
+
+        let creds_lock = self.creds.read()
+            .map_err(|_| CredentialError(CredE::Other("Synchronization error".to_string())))?;
+        let (cred_pub, sign_count) = creds_lock.get(&cred.id)
+            .ok_or(CredentialError(CredE::Other("Credential not found".to_string())))?;
+
+        let mut verifier = CredentialRequestVerifier::new(
+            cred,
+            cred_pub.clone(),
+            context.clone(),
+            "http://localhost",
+            uuid.as_str(),
+            *sign_count,
+        );
+        let res = verifier.verify()?;
+        self.creds.write().unwrap().insert(uuid, (cred_pub.clone(), res.sign_count));
+        Ok((200, "it works".to_string()))
+    }
+}
+
+pub struct CorsMiddleware;
+
+impl CorsMiddleware {
+    pub fn new() -> Self {
+        CorsMiddleware {}
+    }
+}
+
+#[middleware]
+impl CorsMiddleware {
+    // fn resolve(&self, req: &mut SyncRequest, res: &mut SyncResponse) -> RequestContinuation {
+    async fn next(&self, mut ctx: HttpContext, chain: &dyn MiddlewareChain) -> Result<HttpContext, SaphirError> {
+        let req = ctx.state.request_unchecked();
+        let headers = req.headers().clone();
+        let is_auth = req.uri().path().contains("/auth");
+
+        if req.method() == Method::OPTIONS.as_ref() {
+            ctx.after(Builder::new()
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Auth-ID, WWW-Authenticate, auth-id, www-authenticate, authorization, Authorization, Origin, origin, Set-Cookie, set-cookie, Cookie, cookie, Code, Content-Type, content-type")
+                .status(StatusCode::NO_CONTENT)
+                .build()?);
+        } else {
+            ctx = chain.next(ctx).await?;
+        }
+
+        let res = ctx.state.response_unchecked_mut();
+        let res_headers = res.headers_mut();
+
+        if let Some(Ok(origin)) = headers.get("Origin").map(|c| c.to_str()) {
+            res_headers.insert("Access-Control-Allow-Origin", origin.parse()?);
+        } else {
+            res_headers.insert("Access-Control-Allow-Origin", "*".parse()?);
+        }
+
+        res_headers.insert("Access-Control-Expose-Headers", "Auth-ID, WWW-Authenticate, auth-id, www-authenticate, authorization, Authorization, Origin, origin, Set-Cookie, set-cookie, Cookie, cookie".parse()?);
+
+        if is_auth {
+            res_headers.insert("Access-Control-Allow-Credentials", "true".parse()?);
+        }
+
+        Ok(ctx)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), SaphirError> {
+    let server = Server::builder()
+        .configure_middlewares(|stack| stack.apply(CorsMiddleware::new(), vec!["/"], None))
+        .configure_router(|router| {
+            router.controller(TestController::new())
+        })
+        .configure_listener(|listener_config| listener_config.interface("0.0.0.0:12345"))
+        .build();
+
+    server.run().await
+}
+
+pub fn gen_challenge(len: usize) -> String {
+    let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    let mut rng = rand::thread_rng();
+    let value = (0..len).map(|_| charset.chars().choose(&mut rng).unwrap() as u8).collect::<Vec<u8>>();
+    base64::encode_config(value.as_slice(), base64::URL_SAFE_NO_PAD)
 }
