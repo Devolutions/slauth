e2ac2f6148370530247d7968cc9dc91ed5e6d07a
Luc Fauvel <lfauvel@devolutions.net>
2025-09-12 13:32:51 -0400

    Moved key conversion to utils struct for iOS (#113)
    * Moved key conversion to utils struct for iOS
    
    * Use class instead of struct

---
+++ wrappers/swift/classes/SlauthUtils.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public class SlauthUtils {
+     public static func convertPkcs8ToPrivateKey(pkcs8String: String) -> String {
+        let cString = pkcs8_to_custom_private_key(pkcs8String)
+        let privateKey = String(cString: cString!)
+        free(cString)
+        return privateKey
+    }
+
+    public static func convertPrivateKeyToPkcs8(privateKey: String) -> String {
+        let cString = private_key_to_pkcs8_der(privateKey)
+        let pkcs8String = String(cString: cString!)
+        free(cString)
+        return pkcs8String
+    }
+}
