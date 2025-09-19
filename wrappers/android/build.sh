8cc2d0df9411f4d3194b0177c50fd8001454e8de
Sebastien Aubin <spicheaubin@devolutions.net>
2025-09-19 16:27:40 -0400

    Add flasg to be compliant with Android 16kb pages

--- wrappers/android/build.sh
+++ wrappers/android/build.sh
@@ -7,6 +7,7 @@ then
     exit 1
 fi
 
+export RUSTFLAGS="-C link-arg=-Wl,-z,max-page-size=16384 -C link-arg=-Wl,-z,common-page-size=16384"
 export PATH=$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin:$PATH
 CC=aarch64-linux-android21-clang cargo build --target aarch64-linux-android --release --features "android"
 CC=x86_64-linux-android21-clang cargo build --target x86_64-linux-android --release --features "android"
